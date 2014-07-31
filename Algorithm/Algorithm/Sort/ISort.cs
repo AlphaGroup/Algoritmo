fe448d4d9dfa76f4279663eadb09e58d07500c00
feng-zhe <henryzhefeng@gmail.com>
2014-07-31 18:07:31 +0800

    Implement the "Insertion-sort".

---
+++ Algorithm/Algorithm/Sort/ISort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sort
+{
+    /// <summary>
+    /// Assuming T implements IComparable.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface ISort<T>
+    {
+        /// <summary>
+        /// Sort the input list by default comparer.
+        /// </summary>
+        /// <param name="inList"></param>
+        /// <returns></returns>
+        List<T> Sort(List<T> inList);
+
+        ///// <summary>
+        ///// Sort the input list
+        ///// Compare elements by inputed comparer.
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <param name="inList"></param>
+        ///// <param name="comparer"></param>
+        ///// <returns></returns>
+        List<T> Sort(List<T> inList, IComparer<T> comparer);
+    }
+
+}
---
+++ Algorithm/Algorithm/Sort/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sort
+{
+    class InsertSort<T> : ISort<T>
+    {
+        /// <summary>
+        /// Use default comparer.
+        /// Won't change inputed list.
+        /// </summary>
+        /// <param name="inList"></param>
+        /// <returns></returns>
+        public List<T> Sort(List<T> inList)
+        {
+            return Sort(inList, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Use inputed comparer. Won't change inputed list.
+        /// </summary>
+        /// <param name="inList"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public List<T> Sort(List<T> inList, IComparer<T> comparer)
+        {
+            var result = new List<T>(inList);
+            for (int j = 1; j < result.Count; ++j)
+            {
+                var key = result.ElementAt(j);
+                var i = j - 1;
+                while (i >= 0 && comparer.Compare(result[i], key) > 0)
+                {
+                    result[i + 1] = result[i];
+                    --i;
+                }
+                result[i + 1] = key;
+            }
+            return result;
+        }
+    }
+}
