495dc9663061f888acfaffc3976ce326e81dae3e
feng-zhe <henryzhefeng@gmail.com>
2014-08-06 10:17:31 +0800

    Edit .gitignore file to include main() in Algorithm.

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm.Interface;
+using Algorithm.Sort;
+using Algorithm.UnitTest;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnitTest[] tests =
+            {
+                new SortTest(), // Test sort methods
+                new HeapTest(), // Test heap data structure
+            };
+            foreach (var test in tests)
+            {
+                test.Test();
+            }
+            Console.WriteLine("End!");
+            Console.Read();
+        }
+    }
+}
