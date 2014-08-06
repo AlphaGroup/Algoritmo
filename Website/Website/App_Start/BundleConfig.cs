f4096e8df19521d7d836bd69d4443de10cc0e67f
feng-zhe <henryzhefeng@gmail.com>
2014-08-06 17:31:36 +0800

    Animation about exchanging two squares.

--- Website/Website/App_Start/BundleConfig.cs
+++ Website/Website/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace Website
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/raphael").Include(
+                        "~/Scripts/raphael.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
