91595987916093084f97509b13c21a186fc04b9b
feng-zhe <henryzhefeng@gmail.com>
2014-08-07 09:15:17 +0800

    [Refactored]: Add HorizonalExchange function.

---
+++ Website/Website/Scripts/mine/animation.js
@@ -0,0 +1,22 @@
+﻿// Exchange two item horizonally.
+function HorizonalExchange(squareOne, squareTwo) {
+    var oldOneX = squareOne.attr("x");
+    var oldTwoX = squareTwo.attr("x");
+    // Find which one is at left
+    var leftSqua = squareOne;
+    var rightSqua = squareTwo;
+    if (oldOneX > oldTwoX) {
+        leftSqua = squareTwo;
+        rightSqua = squareOne;
+    }
+    var leftX = leftSqua.attr("x");
+    var rightX = rightSqua.attr("x");
+    var leftY = leftSqua.attr("y");
+    leftSqua.animate({ y: leftY - 120 }, 500, function () {
+        this.animate({ x: rightX }, 500, "ease-out", function () {
+            rightSqua.animate({ x: leftX }, 500, "ease-out");
+            var anim = Raphael.animation({ y: leftY }, 500, "ease-out");
+            this.animate(anim.delay(500));
+        });
+    });
+}
