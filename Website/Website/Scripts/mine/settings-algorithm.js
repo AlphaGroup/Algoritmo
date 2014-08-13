dc1479c1744d31df7a0a585db3b66b54987934e1
feng-zhe <henryzhefeng@gmail.com>
2014-08-13 15:21:13 +0800

    Add sort algorithm type into cookie.

--- Website/Website/Scripts/mine/settings-algorithm.js
+++ Website/Website/Scripts/mine/settings-algorithm.js
@@ -1,5 +1,13 @@
-﻿$(function() {
+﻿// Set nav bar
+$(function () {
     $("div#sideNavbar li").removeClass("active");
     $("div#sideNavbar li:contains('Algorithm')").addClass("active");
     $("div#sideNavbar").css("width", "200px");
+});
+
+// Set radio click handler.
+$(function () {
+    $("div.radio :radio").click(function () {
+        $.cookie("algorithm", $(this).val());
+    });
 });
