46fb6d238b2077a8533334018ee65ef846c6768f
feng-zhe <henryzhefeng@gmail.com>
2014-09-09 16:11:00 +0800

    Add test types "double"
    Also add bucketsort file.

---
+++ Algorithm/Algorithm/Sort/BucketSort.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * worst-case: theta(n^2)
+ * average-case: theta(n)
+ * The bucket sort assumes that the inputs are a uniform distribution.
+ * Which means that all inputs belong to [0,1) randomly.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm.Interface;
+
+namespace Algorithm.Sort
+{
+    class BucketSort : ISort<double>
+    {
+        public void Sort(List<double> inList)
+        {
+            Sort(inList, Comparer<double>.Default);
+        }
+
+        public void Sort(List<double> inList, IComparer<double> comparer)
+        {
+            // Check the inputs
+            var max = inList.Max();
+            var min = inList.Min();
+            if (max >= 1 || min < 0)
+            {
+                throw new Exception("Error! Invalid input for bucket sort!");
+            }
+            var arrOfList = new List<double>[inList.Count()];
+        }
+    }
+}
--- Algorithm/Algorithm/UnitTest/SortTest.cs
+++ Algorithm/Algorithm/UnitTest/SortTest.cs
@@ -14,14 +14,15 @@ namespace Algorithm.UnitTest
     {
         public void Test()
         {
+            // Test input of ints
             // The inputs used to test every sort method
-            List<int>[] inputs =
+            List<int>[] intInputs =
             {
                 new List<int>(){5,3,7,8,10,9,2},
                 new List<int>(){3,7,8,10,9,100,2,20,16,13,10,59}
             };
             // The sorts to be tested
-            ISort<int>[] sorts =
+            ISort<int>[] intSorts =
             {
                 new InsertSort<int>(),  // Insertion sort
                 new MergeSort<int>(),   // Merge sort
@@ -32,15 +33,45 @@ namespace Algorithm.UnitTest
                 new SelectionSort<int>(),   // Selection sort
                 new CountingSort(),     // Counting sort
             };
-            foreach (var sort in sorts)
+            foreach (var sort in intSorts)
             {
-                foreach (var input in inputs)
+                foreach (var input in intInputs)
                 {
                     var output = new List<int>(input);
                     sort.Sort(output);
                     var standard = new List<int>(input);
                     standard.Sort();
                     bool flag = ListEqual(output, standard, Comparer<int>.Default);
+                    Assert.AreEqual(true, flag);
+                }
+            }
+            // Test input of doubles
+            // The inputs used to test every sort method
+            List<double>[] doubleInputs =
+            {
+                new List<double>(){5.3,3.5,7.2,8.4,10.1,9.1,2.0},
+                new List<double>(){3.1,7.1,8.6,10.7,9.32,100.234,2.233,20.455,16.765,13.664,10.234,59.999}
+            };
+            // The sorts to be tested
+            ISort<double>[] doubleSorts =
+            {
+                new InsertSort<double>(),  // Insertion sort
+                new MergeSort<double>(),   // Merge sort
+                new BubbleSort<double>(),  // Bubble sort
+                new ShellSort<double>(),   // Shell sort
+                new HeapSort<double>(),    // Heap sort
+                new QuickSort<double>(),   // Quick sort
+                new SelectionSort<double>(),   // Selection sort
+            };
+            foreach (var sort in doubleSorts)
+            {
+                foreach (var input in doubleInputs)
+                {
+                    var output = new List<double>(input);
+                    sort.Sort(output);
+                    var standard = new List<double>(input);
+                    standard.Sort();
+                    bool flag = ListEqual(output, standard, Comparer<double>.Default);
                     Assert.AreEqual(true, flag);
                 }
             }
