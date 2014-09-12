e932f417dee945f023aa665591991949068dcc8a
feng-zhe <henryzhefeng@gmail.com>
2014-09-12 15:25:12 +0800

    Copy code from book
    Still trying to understand RBT's Delete and DeleteFixUp.

--- Algorithm/Algorithm/DataStructure/RedBlackTree.cs
+++ Algorithm/Algorithm/DataStructure/RedBlackTree.cs
@@ -21,6 +21,35 @@ namespace Algorithm.DataStructure
 
         private RedBlackTreeNode<T> _root = RedBlackTreeNode<T>.Nil;
 
+        // Max and min functions
+        // Minimum
+        public T Minimum()
+        {
+            return Minimum(_root).Key;
+        }
+        public RedBlackTreeNode<T> Minimum(RedBlackTreeNode<T> node)
+        {
+            while (node.LeftNode != null)
+            {
+                node = node.LeftNode;
+            }
+            return node;
+        }
+
+        // Maximum
+        public T Maximum()
+        {
+            return Maximum(_root).Key;
+        }
+        public RedBlackTreeNode<T> Maximum(RedBlackTreeNode<T> node)
+        {
+            while (node.RightNode != null)
+            {
+                node = node.RightNode;
+            }
+            return node;
+        }
+
         // Rotation: both run in O(1)
         // Left rotation assuming that input.RightNode!=Nil and that the root's parent is Nil.
         private void LeftRotate(RedBlackTreeNode<T> node)
@@ -87,7 +116,7 @@ namespace Algorithm.DataStructure
         // Insert
         public void Insert(T val)
         {
-            var node = new RedBlackTreeNode<T>()
+            var node = new RedBlackTreeNode<T>
             {
                 Key = val,
             };
@@ -96,9 +125,9 @@ namespace Algorithm.DataStructure
         // This overloaded function only assumes that the new node contains key.
         public void Insert(RedBlackTreeNode<T> newNode)
         {
-            Insert(newNode,Comparer<T>.Default);
+            Insert(newNode, Comparer<T>.Default);
         }
-        public void Insert(RedBlackTreeNode<T> newNode,IComparer<T> comparer)
+        public void Insert(RedBlackTreeNode<T> newNode, IComparer<T> comparer)
         {
             var parent = RedBlackTreeNode<T>.Nil;
             var temp = _root;
@@ -124,18 +153,216 @@ namespace Algorithm.DataStructure
             newNode.LeftNode = RedBlackTreeNode<T>.Nil;
             newNode.RightNode = RedBlackTreeNode<T>.Nil;
             newNode.Color = NodeColor.Red;
-            InsertFixUp();
+            InsertFixUp(newNode);
         }
 
         // Recolor nodes and perform rotations in order to restore RBT properties.
-        private void InsertFixUp()
+        private void InsertFixUp(RedBlackTreeNode<T> inserted)
         {
-            
+            var node = inserted;
+            // Loop as long as node's parent is red.
+            while (node.ParentNode.Color == NodeColor.Red)
+            {
+                // The parent is the left child of grandfather
+                // We distinguish this because we have to find the uncle.
+                if (node.ParentNode == node.ParentNode.ParentNode.LeftNode)
+                {
+                    // Find uncle
+                    var uncle = node.ParentNode.ParentNode.RightNode;
+                    // Case 1: uncle is red.
+                    if (uncle.Color == NodeColor.Red)
+                    {
+                        // Then we set parent and uncle to be black and set grandpa red.
+                        node.ParentNode.Color = NodeColor.Black;
+                        uncle.Color = NodeColor.Black;
+                        node.ParentNode.ParentNode.Color = NodeColor.Red;
+                        node = node.ParentNode.ParentNode;
+                    }
+                    // Uncle is black.
+                    else
+                    {
+                        // Case 2
+                        if (node == node.ParentNode.RightNode)
+                        {
+                            // Then we transform the situation into case 3.
+                            node = node.ParentNode;
+                            LeftRotate(node);
+                        }
+                        // Case 3
+                        node.ParentNode.Color = NodeColor.Black;
+                        node.ParentNode.ParentNode.Color = NodeColor.Red;
+                        RightRotate(node.ParentNode.ParentNode);
+                    }
+                }
+                // The parent is the right child of grandfather
+                // The rest code is similiar to the if clause. Only exchang right and left
+                else
+                {
+                    var uncle = node.ParentNode.ParentNode.LeftNode;
+                    if (uncle.Color == NodeColor.Red)
+                    {
+                        node.ParentNode.Color = NodeColor.Black;
+                        uncle.Color = NodeColor.Black;
+                        node.ParentNode.ParentNode.Color = NodeColor.Red;
+                        node = node.ParentNode.ParentNode;
+                    }
+                    else
+                    {
+                        if (node == node.ParentNode.LeftNode)
+                        {
+                            node = node.ParentNode;
+                            RightRotate(node);
+                        }
+                        node.ParentNode.Color = NodeColor.Black;
+                        node.ParentNode.ParentNode.Color = NodeColor.Red;
+                        LeftRotate(node.ParentNode.ParentNode);
+                    }
+                }
+            }
+            _root.Color = NodeColor.Black;
         }
 
+        // Delete
+        // This function is the most complicated function of RBT.
+        public void Delete(RedBlackTreeNode<T> z)
+        {
+            // TODO: Analyze and change variables' names
+            var y = z;
+            var yOldColor = y.Color;
+            RedBlackTreeNode<T> x = null;
+            if (z.LeftNode == RedBlackTreeNode<T>.Nil)
+            {
+                x = z.RightNode;
+                Transplant(z, z.RightNode);
+            }
+            else if (z.RightNode == RedBlackTreeNode<T>.Nil)
+            {
+                x = z.LeftNode;
+                Transplant(z, z.LeftNode);
+            }
+            else
+            {
+                y = Minimum(z.RightNode);
+                yOldColor = y.Color;
+                x = y.RightNode;
+                if (y.ParentNode == z)
+                {
+                    x.ParentNode = y;
+                }
+                else
+                {
+                    Transplant(y, y.RightNode);
+                    y.RightNode = z.RightNode;
+                    y.RightNode.ParentNode = y;
+                }
+                Transplant(z, y);
+                y.LeftNode = z.LeftNode;
+                y.LeftNode.ParentNode = y;
+                y.Color = z.Color;
+            }
+            if (yOldColor == NodeColor.Black)
+            {
+                DeleteFixUp(x);
+            }
+        }
+        // Helper functions for Deletion
+        private void Transplant(RedBlackTreeNode<T> replaced, RedBlackTreeNode<T> replacer)
+        {
+            if (replaced.ParentNode == RedBlackTreeNode<T>.Nil)
+            {
+                _root = replacer;
+            }
+            else if (replaced == replaced.ParentNode.LeftNode)
+            {
+                replaced.ParentNode.LeftNode = replacer;
+            }
+            else
+            {
+                replaced.ParentNode.RightNode = replacer;
+            }
+            replacer.ParentNode = replaced.ParentNode;
+        }
+
+        private void DeleteFixUp(RedBlackTreeNode<T> x)
+        {
+            // TODO: Analyze and change variables' names.
+            while (x != _root && x.Color == NodeColor.Black)
+            {
+                if (x == x.ParentNode.LeftNode)
+                {
+                    var w = x.ParentNode.RightNode;
+                    // Case 1
+                    if (w.Color == NodeColor.Red)
+                    {
+                        w.Color = NodeColor.Black;
+                        x.ParentNode.Color = NodeColor.Red;
+                        LeftRotate(x.ParentNode);
+                        w = x.ParentNode.RightNode;
+                    }
+                    // Case 2
+                    if (w.LeftNode.Color == NodeColor.Black && w.RightNode.Color == NodeColor.Black)
+                    {
+                        w.Color = NodeColor.Red;
+                        x = x.ParentNode;
+                    }
+                    else
+                    {
+                        if (w.RightNode.Color == NodeColor.Black)
+                        {
+                            w.LeftNode.Color = NodeColor.Black;
+                            w.Color = NodeColor.Red;
+                            RightRotate(w);
+                            w = x.ParentNode.RightNode;
+                        }
+                        w.Color = x.ParentNode.Color;
+                        x.ParentNode.Color = NodeColor.Black;
+                        w.RightNode.Color = NodeColor.Black;
+                        LeftRotate(x.ParentNode);
+                        x = _root;
+                    }
+                }
+                // Same with "if" part with "right" and "left" exchanged.
+                else
+                {
+                    var w = x.ParentNode.LeftNode;
+                    // Case 1
+                    if (w.Color == NodeColor.Red)
+                    {
+                        w.Color = NodeColor.Black;
+                        x.ParentNode.Color = NodeColor.Red;
+                        RightRotate(x.ParentNode);
+                        w = x.ParentNode.LeftNode;
+                    }
+                    // Case 2
+                    if (w.RightNode.Color == NodeColor.Black && w.LeftNode.Color == NodeColor.Black)
+                    {
+                        w.Color = NodeColor.Red;
+                        x = x.ParentNode;
+                    }
+                    else
+                    {
+                        if (w.LeftNode.Color == NodeColor.Black)
+                        {
+                            w.RightNode.Color = NodeColor.Black;
+                            w.Color = NodeColor.Red;
+                            LeftRotate(w);
+                            w = x.ParentNode.LeftNode;
+                        }
+                        w.Color = x.ParentNode.Color;
+                        x.ParentNode.Color = NodeColor.Black;
+                        w.LeftNode.Color = NodeColor.Black;
+                        RightRotate(x.ParentNode);
+                        x = _root;
+                    }
+                }
+            }
+            x.Color = NodeColor.Black;
+        }
+
+        // The class of RBT's node.
         public class RedBlackTreeNode<TP>
         {
-            // The nil node representing leaves
+            // The nil node representing leaves. Its properties have no meanings except Color.
             public static RedBlackTreeNode<TP> Nil = new RedBlackTreeNode<TP>
             {
                 Color = NodeColor.Black,
