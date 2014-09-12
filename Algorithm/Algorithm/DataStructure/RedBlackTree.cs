2adcf5d782f70f29b7c8ea8bd224d7f74ab54da1
feng-zhe <henryzhefeng@gmail.com>
2014-09-12 09:49:06 +0800

    Add two files of RBT

---
+++ Algorithm/Algorithm/DataStructure/RedBlackTree.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * P309
+ * Compared to BST, RBT is balanced in order to guarantee that basic dynamic-set operations take O(lgn) time in worst case.
+ */
+
+namespace Algorithm.DataStructure
+{
+    // TODO: Here I want to use interface IDynamicSet and inheritance from BST.
+    // TODO: But I have to solve the problem about return value type. Further design is needed.
+    internal class RedBlackTree<T>
+    {
+        public enum NodeColor
+        {
+            Red,
+            Black
+        }
+
+        public class RedBlackTreeNode<TP> : BinarySearchTree<TP>.TreeNode<TP>
+        {
+            public NodeColor Color { get; set; }
+        }
+
+    }
+}
---
+++ Algorithm/Algorithm/UnitTest/RedBlackTreeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm.Interface;
+
+namespace Algorithm.UnitTest
+{
+    class RedBlackTreeTest : IUnitTest
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
