66ef8f07844a01df39fcadd6575ad0ffbb87bcdf
feng-zhe <henryzhefeng@gmail.com>
2014-09-15 18:50:59 +0800

    Realize more code of Order static tree
    Only Insert and Delete function to be implement

--- Algorithm/Algorithm/DataStructure/OSRBTree.cs
+++ Algorithm/Algorithm/DataStructure/OSRBTree.cs
@@ -20,63 +20,79 @@ namespace Algorithm.DataStructure
             };
         }
 
+        // The root node
         private OSRBTreeNode<T> _root = null;
 
+        // Min and Max
         new public T Minimum()
         {
             return Minimum(_root).Key;
         }
-
         public OSRBTreeNode<T> Minimum(OSRBTreeNode<T> root)
         {
             return (OSRBTreeNode<T>)base.Minimum(root);
         }
-
+        new public T Maximum()
+        {
+            return Maximum(_root).Key;
+        }
         public OSRBTreeNode<T> Maximum(OSRBTreeNode<T> root)
         {
             return (OSRBTreeNode<T>)base.Maximum(root);
         }
 
-        public new OSRBTreeNode<T> Search(T key)
+        // Search
+        // Use father's search function.
+        new public OSRBTreeNode<T> Search(T key)
         {
-            throw new NotImplementedException();
+            return (OSRBTreeNode<T>)base.Search(_root, key, Comparer<T>.Default);
         }
-
-        OSRBTreeNode<T> IDynamicSet<T, OSRBTreeNode<T>>.Search(T key, IComparer<T> comparer)
-        {
-            throw new NotImplementedException();
-        }
-
         public OSRBTreeNode<T> Search(OSRBTreeNode<T> root, T key)
         {
-            throw new NotImplementedException();
+            return (OSRBTreeNode<T>)base.Search(root, key, Comparer<T>.Default);
         }
-
+        new public OSRBTreeNode<T> Search(T key, IComparer<T> comparer)
+        {
+            return (OSRBTreeNode<T>)base.Search(_root, key, comparer);
+        }
         public OSRBTreeNode<T> Search(OSRBTreeNode<T> root, T key, IComparer<T> comparer)
         {
-            throw new NotImplementedException();
+            return (OSRBTreeNode<T>)base.Search(root, key, comparer);
         }
 
+        // Successor and Predecessor
         public OSRBTreeNode<T> Successor(OSRBTreeNode<T> root)
         {
-            throw new NotImplementedException();
+            return (OSRBTreeNode<T>)base.Successor(root);
         }
-
         public OSRBTreeNode<T> Predecessor(OSRBTreeNode<T> root)
         {
-            throw new NotImplementedException();
+            return (OSRBTreeNode<T>)base.Predecessor(root);
         }
 
+        // Insert
+        new public void Insert(T newVal)
+        {
+            Insert(newVal, Comparer<T>.Default);
+        }
+        new public void Insert(T newVal, IComparer<T> comparer)
+        {
+            var node = new OSRBTreeNode<T>()
+            {
+                Key = newVal
+            };
+            Insert(node, comparer);
+        }
         public void Insert(OSRBTreeNode<T> newNode)
         {
-            throw new NotImplementedException();
+            Insert(newNode, Comparer<T>.Default);
         }
-
         public void Insert(OSRBTreeNode<T> newNode, IComparer<T> comparer)
         {
             throw new NotImplementedException();
         }
 
+        // Delete
         public void Delete(OSRBTreeNode<T> node)
         {
             throw new NotImplementedException();
